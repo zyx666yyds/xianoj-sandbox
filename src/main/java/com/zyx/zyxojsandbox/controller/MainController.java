7e29b952a3f63fe89fddfc9276d9c60024907e87
迷寂 <2408893294@qq.com>
2024-01-11 20:31:07 +0800

    增加请求头和密钥加密

--- src/main/java/com/zyx/zyxojsandbox/controller/MainController.java
+++ src/main/java/com/zyx/zyxojsandbox/controller/MainController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author zyx
@@ -17,6 +19,15 @@ import javax.annotation.Resource;
  */
 @RestController("/")
 public class MainController {
+
+    /**
+     * 请求头
+     */
+    private static final String AUTH_REQUEST_HEADER = "auth";
+    /**
+     * 密钥
+     */
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
 
     @Resource
     private JavaNativeCodeSandbox javaNativeCodeSandbox;
@@ -28,12 +39,21 @@ public class MainController {
 
     /**
      * 开放api
+     *
      * @param executeCodeRequest
      * @return
      */
     @PostMapping("/executeCode")
-    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest) {
-        if (executeCodeRequest == null){
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest
+            , HttpServletRequest request, HttpServletResponse response) {
+        //基本认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!authHeader.equals(AUTH_REQUEST_SECRET)) {
+            response.setStatus(403);
+            return null;
+        }
+
+        if (executeCodeRequest == null) {
             throw new NullPointerException("executeCodeRequest is null");
         }
         return javaNativeCodeSandbox.executeCode(executeCodeRequest);
